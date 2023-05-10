a03bc8650e7573d580a884dd46b03c8269c45300
Barneygordons <barnygordons@gmail.com>
2023-05-10 11:57:27 +0000

    Made a few changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
-
-f subscribers"""
+#!/usr/bin/python3
+""" total of subscribers"""
 
 import requests
 
@@ -9,7 +9,7 @@ base_url = 'http://reddit.com/r/{}/about.json'
 
 def number_of_subscribers(subreddit):
     """number of suscribers"""
-    headers = {'User-agent': 'karenahv'}
+    headers = {'User-agent': 'Barneygordons'}
     req = requests.get(base_url.format(subreddit), headers=headers)
     if req.status_code != 200:
         return 0
